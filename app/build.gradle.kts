cee0046c2520dbf14fa8b526f3bde750b849e9e3
Chetan45m <sasha000158@gmail.com>
2026-04-29 19:51:24 +0300

    Fix resources

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,8 +2,8 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("kotlin-kapt")
-    id("com.google.gms.google-services")
-    id("com.google.firebase.crashlytics")
+//    id("com.google.gms.google-services")
+//    id("com.google.firebase.crashlytics")
 }
 
 android {
@@ -40,6 +40,7 @@ android {
 
     buildFeatures {
         dataBinding = true
+        buildConfig = true
         viewBinding = true
     }
     compileOptions {
@@ -117,6 +118,13 @@ dependencies {
 
     implementation("androidx.viewpager:viewpager:1.0.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
-
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("io.github.scwang90:refresh-layout-kernel:3.0.0-alpha")
+    implementation("io.github.scwang90:refresh-header-classics:3.0.0-alpha")
+    implementation("io.github.scwang90:refresh-footer-classics:3.0.0-alpha")
+    implementation("androidx.mediarouter:mediarouter:1.8.1")
+    implementation("com.google.android.exoplayer:exoplayer:2.19.1")
+    implementation("com.google.android.exoplayer:exoplayer-ui:2.19.1")
 //    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar", "*.aar"))))
 }
